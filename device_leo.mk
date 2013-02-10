71ab8b0f366bd5a39c91ca2be0deb8fdbd80ea3b
sportsstar89 <sjpirri@gmail.com>
2013-02-10 01:35:49 -0500

    more cleanup

--- device_leo.mk
+++ device_leo.mk
@@ -90,7 +90,7 @@ PRODUCT_COPY_FILES += \
 	device/htc/leo/clk/etc/init.d/97ppp:system/etc/init.d/97ppp
 
 # Kernel modules
-ifeq ($(TARGET_PREBUILT_KERNEL),)
+ifeq ($(TARGET_PREBUILT_KERNEL_USED)true,)
 PRODUCT_COPY_FILES += $(shell \
     find device/htc/leo/modules -name '*.ko' \
     | sed -r 's/^\/?(.*\/)([^/ ]+)$$/\1\2:system\/lib\/modules\/\2/' \
