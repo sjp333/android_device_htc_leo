b26524b4ae371bac5029807f073c45de39fc72dd
Sportsstar89 <sjpirri@gmail.com>
2013-02-17 16:09:14 -0500

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -18,5 +18,5 @@ PRODUCT_NAME := cm_leo
 PRODUCT_CODENAME := leo
 
 # Set build fingerprint / ID / Product Name ect.
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=leo BUILD_ID=JRO03L BUILD_DISPLAY_ID=JRO03L BUILD_FINGERPRINT="google/htc_leo/leo:4.1.1/JRO03L/239410:user/release-keys" PRVIATE_BUILD_DESC="full_leo-user 4.1.1 JRO03L 239410 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=leo BUILD_FINGERPRINT=google/htc_leo/leo:4.2.2/JDQ39/573038:user/release-keys PRIVATE_BUILD_DESC="leo-user 4.2.2 JDQ39 573038 release-keys"
 
