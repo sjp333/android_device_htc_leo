7a0cd89d2657cdc92fa3fc884052ae10e79300fe
walter79 <gzwalter79@gmail.com>
2014-05-10 08:49:33 +0200

    revert update kernel base

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -59,7 +59,7 @@
   TARGET_KERNEL_SOURCE := kernel/htc/leo
   TARGET_KERNEL_CONFIG := htcleo_defconfig
   BOARD_KERNEL_CMDLINE := no_console_suspend=1 wire.search_count=5
-  BOARD_KERNEL_BASE := 0x11800000
+  BOARD_KERNEL_BASE := 0x11808000
   BOARD_KERNEL_NEW_PPPOX := true
   TARGET_KERNEL_CUSTOM_TOOLCHAIN := arm-eabi-4.4.3
   
