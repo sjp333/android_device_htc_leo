744e184823864be4986cc081920faed51defe880
walter79 <gzwalter79@gmail.com>
2014-05-30 19:06:55 +0200

    first update for evervolv

--- ev.mk
+++ ev.mk
@@ -1,9 +1,9 @@
 # Inherit GSM configuration
-$(call inherit-product, vendor/omni/config/gsm.mk)
+$(call inherit-product, vendor/ev/config/gsm.mk)
 # Inherit Omni Product configuration
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, $(SRC_EVERVOLV_DIR)/config/common_small_phone.mk)
 # Inherit Leo Product configuration
 $(call inherit-product, device/htc/leo/full_leo.mk)
 
-PRODUCT_NAME := omni_leo
+PRODUCT_NAME := ev_leo
 PRODUCT_DEVICE := leo
