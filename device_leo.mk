febd9ead3f7be7dea22f7247314d5a2a46a298e7
Michael Bestas <mikeioannina@gmail.com>
2015-05-31 17:46:31 +0300

    Set dalvik.vm.dex2oat-filter=balanced
    * ~11x runtime performance improvement
    
    Change-Id: I108f10518637334fea65351c54f3d3ad2a0de5e2

--- device_leo.mk
+++ device_leo.mk
@@ -147,7 +147,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	rw.media.record.hasb=0
 
 PRODUCT_PROPERTY_OVERRIDES += \
-	dalvik.vm.dex2oat-filter=interpret-only \
+	dalvik.vm.dex2oat-filter=balanced \
 	dalvik.vm.dex2oat-flags=--no-watch-dog \
 	dalvik.vm.image-dex2oat-filter=speed
 
